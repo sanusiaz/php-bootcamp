6b7531d65c3d9407a6f21665c952a259b8f860a2
sanusiaz <sanusiabdulazeezadebayo1@gmail.com>
2022-12-30 18:11:51 +0100

    First php code

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Php tutorial</title>
+</head>
+<body>
+
+    <?php
+
+        echo "Hello world";
+
+        echo "<br>";
+        echo 'Hello world 2';
+
+        
+    ?>
+</body>
+</html>
